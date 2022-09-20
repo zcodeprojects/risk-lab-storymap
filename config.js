b0dcde511e408196da8a9b0104e9b7d2f612308a
Zion Michael <62163365+zcodeprojects@users.noreply.github.com>
2022-09-20 01:54:58 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,111 @@
+var config = {
+    style: 'mapbox://styles/mapbox/streets-v11',
+    accessToken: 'pk.eyJ1Ijoiem1pY2hhZWwiLCJhIjoiY2w1andveHNoMDRjaTNidGV6Y20wenowMCJ9.7zPB-Hfyzc0Xor01Y6AiVw',
+    showMarkers: true,
+    markerColor: '#3FB1CE',
+    //projection: 'equirectangular',
+    //Read more about available projections here
+    //https://docs.mapbox.com/mapbox-gl-js/example/projections/
+    inset: true,
+    theme: 'dark',
+    use3dTerrain: false, //set true for enabling 3D maps.
+    title: 'The Title Text of this Story',
+    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
+    byline: 'By a Digital Storyteller',
+    footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    chapters: [
+        {
+            id: 'slug-style-id',
+            alignment: 'left',
+            hidden: false,
+            title: 'Contaminated Sites Map',
+            image: './path/to/image/source.png',
+            description: 'This map shows contaminated sites in broward county.',
+            location: {
+                center: [-122.418398, 37.759483],
+                zoom: 8.5,
+                pitch: 60,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'second-identifier',
+            alignment: 'right',
+            hidden: false,
+            title: 'Second Title',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [-77.020636, 38.886900],
+                zoom: 8.5,
+                pitch: 60,
+                bearing: -43.2,
+                // flyTo additional controls-
+                // These options control the flight curve, making it move
+                // slowly and zoom out almost completely before starting
+                // to pan.
+                //speed: 2, // make the flying slow
+                //curve: 1, // change the speed at which it zooms out
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'third-identifier',
+            alignment: 'left',
+            hidden: false,
+            title: 'Third Title',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [6.15116, 46.20595],
+                zoom: 12.52,
+                pitch: 8.01,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'fourth-chapter',
+            alignment: 'fully',
+            hidden: false,
+            title: 'Third Title',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [-58.54195, -34.71600],
+                zoom: 4,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ]
+};
